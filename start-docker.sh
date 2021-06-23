8f56f2e16c5518e09ff0cdbd9f79df370e7e249e
Belliato Riccardo <belliato.riccardo@spes.uniud.it>
2021-06-23 11:38:19 +0200

    tag for image

--- start-docker.sh
+++ start-docker.sh
@@ -1,5 +1,6 @@
 # $1: path alla webcam
 # $2: porta TCP
 # $3: path alla directory con il file di configurazione motion.conf
+# $4: tag dell'immagine docker
 
-docker run -itd --name=motion --device $1 -p $2:$2 -v $3:/etc/motion rik1599/motion-docker
+docker run -itd --name=motion --device $1 -p $2:$2 -v $3:/etc/motion rik1599/motion-docker:$4
